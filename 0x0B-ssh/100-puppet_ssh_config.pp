5bbb3bcb1892f7bbbe97ccaa578192f5b4c60bf8
exuszee <exuszee@gmail.com>
2023-04-22 17:38:24 +0300

    4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,13 @@
-#!/usr/bin/env bash
+# Sets up a client SSH configuration file so that you can connect to a server without typing a password.
+file { '/home/ubuntu/.ssh/ssh_config':
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => '
+  Host myserver
+      HostName 100.25.36.86
+      User ubuntu
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  '
+}
