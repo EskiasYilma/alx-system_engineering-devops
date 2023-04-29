7c2fe27d0ad7a5fefa9d457059c2754e31ecfde2
exuszee <exuszee@gmail.com>
2023-04-29 19:24:20 +0300

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,9 +11,43 @@ package { 'nginx':
   install_options => ['-y']
 }
 
+# html directory
+file { '/var/www/html':
+  ensure => 'directory',
+  mode   => '0755',
+  owner  => 'root',
+  group  => 'root',
+}
+
+# index.html content
+file { '/var/www/html/index.html':
+  ensure  => 'file',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  content => 'Hello World!\n',
+  require => File['/var/www/html'],
+}
+
+# 404.html content
+file { '/var/www/html/404.html':
+  ensure  => 'file',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  content => "Ceci n'est pas une page\n",
+  require => File['/var/www/html'],
+}
+
 # Redirect
 exec { 'redirect':
   command  => 'sudo sed -i "54i\rewrite ^/redirect_me http://bachmanity.tech permanent;" /etc/nginx/sites-enabled/default',
+  provider => 'shell',
+}
+
+# 404
+exec { '404':
+  command  => 'sudo sed -i "55i\error_page 404 /404.html;" /etc/nginx/sites-enabled/default',
   provider => 'shell',
 }
 
