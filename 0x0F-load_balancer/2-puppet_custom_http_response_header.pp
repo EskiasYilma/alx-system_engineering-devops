9717488e898c1163fd6cc9f2c9c24ef92191fa42
exuszee <exuszee@gmail.com>
2023-04-29 19:10:51 +0300

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,14 +11,20 @@ package { 'nginx':
   install_options => ['-y']
 }
 
+# Redirect
+exec { 'redirect':
+  command  => 'sudo sed -i "54i\rewrite ^/redirect_me http://bachmanity.tech permanent;" /etc/nginx/sites-enabled/default',
+  provider => 'shell',
+}
+
 # X-Served-By
 exec { 'X-Served-By':
-  command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i "16i\add_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf',
   provider => 'shell',
 }
 
 # Restart Nginx
-exec { 'Start Nginx':
-  command => 'service nginx restart',
-  path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin']
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
