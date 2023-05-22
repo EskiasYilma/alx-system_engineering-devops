f2a16800909a83d8741c15ebf3a1533a155ff1cd
exuszee <exuszee@gmail.com>
2023-05-22 19:25:12 +0300

    2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """
-The script must display on the standard output the employee TODO list \
-progress in this exact format:
+Python script to export data in the CSV format.
 
-    First line: Employee EMPLOYEE_NAME is done with \
-    tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-        EMPLOYEE_NAME: name of the employee
-        NUMBER_OF_DONE_TASKS: number of completed tasks
-        TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of \
-        completed and non-completed tasks
-    Second and N next lines display the title of completed tasks: \
-    TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+Requirements:
+
+    Records all tasks that are owned by this employee
+    Format must be: "USER_ID","USERNAME",\
+    "TASK_COMPLETED_STATUS","TASK_TITLE"
+    File name must be: USER_ID.csv
 
 """
 import requests
 import sys
-import urllib
 
 
 if __name__ == "__main__":
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,45 @@
 #!/usr/bin/python3
+"""
+Python script to export data in the CSV format.
+
+Requirements:
+
+    Records all tasks that are owned by this employee
+    Format must be: "USER_ID","USERNAME",\
+    "TASK_COMPLETED_STATUS","TASK_TITLE"
+    File name must be: USER_ID.csv
+
+"""
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+    user_id = str(sys.argv[1])
+    url = 'https://jsonplaceholder.typicode.com/users'
+    end_points = ["posts", "comments", "albums",
+                  "photos", "todos", "users"]
+
+    r = requests.get(url).json()
+    name = None
+    for i in r:
+        if i.get('id') == int(user_id):
+            name = i['name']
+    td = requests.get("http://jsonplaceholder.typicode.com/todos").json()
+    data = {}
+    with open("{}.json".format(user_id), 'w') as f:
+
+        temp_tasks = []
+        for i in td:
+            if i.get("userId") == int(user_id):
+                t_csv = '"{}","{}","{}","{}"'.format(i.get("userId"),
+                                                     name,
+                                                     i.get("completed"),
+                                                     i.get("title"))
+                task = {"task": i.get("title"),
+                        "completed": i.get("completed"),
+                        "username": name}
+                temp_tasks.append(task)
+        data[str(user_id)] = temp_tasks
+        json.dump(data, f)
