ac58c05a5738579fb777c6448414dfee13085508
exuszee <exuszee@gmail.com>
2023-05-22 17:20:29 +0300

    0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,31 +3,38 @@
 A script that, using this REST API, for a given employee ID, \
 returns information about his/her TODO list progress.
 """
-
 import requests
 import urllib
 import sys
 
-if len(sys.argv) < 2:
-    sys.exit(0)
-else:
-    user_id = str(sys.argv[1])
+def api_0():
+    """
+    A script that, using this REST API, for a given employee ID, \
+    returns information about his/her TODO list progress.
+    """
+    if len(sys.argv) < 2:
+        sys.exit(0)
+    else:
+        user_id = str(sys.argv[1])
 
-end_points = ["posts", "comments", "albums", "photos",
-              "todos", "users"]
-employee_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                             format(user_id)).json()['name']
-todos_all = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.
-                         format(user_id)).json()
-total_tasks = len(todos_all)
-completed_tasks = [i['title'] for i in todos_all if i['completed'] is True]
-tcp = len(completed_tasks)
-output = "Employee {} is done with tasks({}/{}):\n\t ".format(
-                                                              employee_name,
-                                                              tcp,
-                                                              total_tasks)
-for i, j in enumerate(completed_tasks):
-    output = output + j
-    if i < len(completed_tasks)-1:
-        output += "\n\t "
-print(output)
+    end_points = ["posts", "comments", "albums", "photos",
+                  "todos", "users"]
+    employee_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                                 format(user_id)).json()['name']
+    todos_all = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.
+                             format(user_id)).json()
+    total_tasks = len(todos_all)
+    completed_tasks = [i['title'] for i in todos_all if i['completed'] is True]
+    tcp = len(completed_tasks)
+    output = "Employee {} is done with tasks({}/{}):\n\t ".format(
+                                                                  employee_name,
+                                                                  tcp,
+                                                                  total_tasks)
+    for i, j in enumerate(completed_tasks):
+        output = output + j
+        if i < len(completed_tasks)-1:
+            output += "\n\t "
+    print(output)
+
+if __name__ == "__main__":
+    api_0()
