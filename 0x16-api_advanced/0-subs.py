320735fb099726901a2effc89a948cafd9732afa
exuszee <exuszee@gmail.com>
2023-06-06 15:29:29 +0300

    Now this is fun

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 import requests
 """
-a function that queries the Reddit API and returns the number\
- of subscribers (not active users, total subscribers) for a \
- given subreddit. If an invalid subreddit is given, the \
- function should return 0.
+Module to return reddit api
 """
 
 
