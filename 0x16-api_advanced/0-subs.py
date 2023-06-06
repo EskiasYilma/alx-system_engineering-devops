e602a9e4393a6c653f2f42455ec17ed8b7881d57
exuszee <exuszee@gmail.com>
2023-06-06 15:27:27 +0300

    Now this is fun

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,17 +18,6 @@ def number_of_subscribers(subreddit):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:113.0) \
                        Gecko/20100101 Firefox/113.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;\
-                   q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'DNT': '1',
-        'Upgrade-Insecure-Requests': '1',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'cross-site',
-        'Connection': 'keep-alive',
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache',
     }
 
     r = requests.get("https://www.reddit.com/r/{}/about.json"
