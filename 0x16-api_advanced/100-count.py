d6ca92f2e0da0f2cb569e9a0befa50aa5545a2e5
exuszee <exuszee@gmail.com>
2023-06-06 17:31:13 +0300

    Super Awesome Task So far!!!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,1 +1,57 @@
 #!/usr/bin/python3
+"""
+Module for reddit api
+"""
+import requests
+
+
+def count_words(subreddit, wordlist, hot_list=[], after=None, count=0):
+    """
+    A recursive function that queries the Reddit API, parses \
+    the title of all hot articles, and prints a sorted count \
+    of given keywords (case-insensitive, delimited by spaces.
+    """
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:113.0) \
+                       Gecko/20100101 Firefox/113.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;\
+                   q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'DNT': '1',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'cross-site',
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+    }
+
+    r = requests.get("https://www.reddit.com/r/{}/.json?sort={}\
+                      &limit={}&count={}&after={}"
+                     .format(str(subreddit), "hot", 100, count, after),
+                     headers=headers)
+    try:
+        dt = r.json().get('data').get('children')
+        for x in dt:
+            hot_list.append(x.get('data').get('title'))
+        after = r.json().get('data').get('after')
+        count += len(dt)
+        if after:
+            count_words(subreddit, wordlist, hot_list, after, count)
+        else:
+            wd = {}
+
+            for i in wordlist:
+                wd[i] = 0
+
+            for i in wordlist:
+                for j in hot_list:
+                    if str(i) in str(j).lower().split(' '):
+                        wd[i] += 1
+            sorted_hot = sorted(wd.items(), key=lambda v: v[1], reverse=True)
+            for i in sorted_hot:
+                print("{}: {}".format(i[0], i[1]))
+
+    except Exception:
+        print("")
